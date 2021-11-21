7d5675598ff33e2613fe91100b317ce7160156e7
Mike Bannister <notimpossiblemike@gmail.com>
2021-11-21 16:40:09 -0500

    Move calculation to fn

--- show_case_and_pcb.py
+++ show_case_and_pcb.py
@@ -8,16 +8,7 @@ def midpoint(p1, p2):
     return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)
 
 
-if "show_object" in globals():
-    [keyboard_parts, geometry] = make_keyboard_parts()
-
-    upper_right_hand_switch_center = midpoint(
-        geometry.switch_plate.points[-1][0],
-        geometry.switch_plate.points[-1][2],
-    )
-
-    [pcb_parts, board_data] = make_pcb_parts()
-
+def calculate_position_of_pcb(board_data):
     edge_cut_lines = [
         line for line in board_data["gr_lines"] if line["layer"] == "Edge.Cuts"
     ]
@@ -44,6 +35,21 @@ if "show_object" in globals():
     )
     x_offset = midpoint_of_pbc[0] - geometry.mirror_at.point[0]
 
+    return [-x_offset, -y_offset, 70]
+
+
+if "show_object" in globals():
+    [keyboard_parts, geometry] = make_keyboard_parts()
+
+    upper_right_hand_switch_center = midpoint(
+        geometry.switch_plate.points[-1][0],
+        geometry.switch_plate.points[-1][2],
+    )
+
+    [pcb_parts, board_data] = make_pcb_parts()
+
+    position_of_pcb = calculate_position_of_pcb(board_data)
+
     keyboard_parts = explode_parts(keyboard_parts, 25)
 
     for layer_name_part_and_options in keyboard_parts:
@@ -53,7 +59,7 @@ if "show_object" in globals():
     for layer_name_part_and_options in pcb_parts:
         [layer_name, part, options] = layer_name_part_and_options
         show_object(
-            part.translate([-x_offset, -y_offset, 70]),
+            part.translate(position_of_pcb),
             name=layer_name,
             options=options,
         )
