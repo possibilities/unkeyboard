655703d952a14abc167bf431ccf4cb6a1dd3feca
Mike Bannister <notimpossiblemike@gmail.com>
2021-12-05 13:44:44 -0500

    Allow fuse_parts to work with non solids

--- fuse_parts.py
+++ fuse_parts.py
@@ -1,11 +1,12 @@
 import cadquery as cq
+from flatten_list import flatten_list
 
 
 def fuse_parts(parts):
     if not len(parts):
         return cq.Workplane()
 
-    part_values = [part.val() for part in parts]
+    part_values = flatten_list([part.vals() for part in parts])
     fused_parts = part_values[0].fuse(*part_values[1:], glue=True).clean()
     fused_object = cq.Workplane().newObject([fused_parts])
 
